7a74beace61fc73687d9f924abe0559f4942b780
saurabh <saurabhj8087445@gmail.com>
2021-12-10 23:31:52 +0530

    add a post controller and router

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ const homeController = require('../controllers/home_controller');
 
 // console.log('router loaded');
 
-router.get('/',homeController.home);
+router.get('/', homeController.home);
+
+router.use('/users', require('./users'));
+
+router.use('/posts', require('./posts'));
+
+// for any further routes, access from here 
+// router.use('/routerName', require('./routerFile'));
+
 
 module.exports = router;
